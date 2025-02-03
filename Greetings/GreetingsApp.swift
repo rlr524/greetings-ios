38f4815e10fde89edaa26947d7c9845d4ebe0a2b
Rob Ranf <rob@emiyaconsulting.com>
2025-02-03 10:07:11 -0800

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Rob Ranf on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
