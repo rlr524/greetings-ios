3041b919552df81f889d36644fba5622b9558c0d
Rob Ranf <rob@emiyaconsulting.com>
2025-02-03 10:33:01 -0800

    init

---
+++ Greetings/Views/MadisonView.swift
@@ -0,0 +1,18 @@
+//
+//  MadisonView.swift
+//  Greetings
+//
+//  Created by Rob Ranf on 2/3/25.
+//
+
+import SwiftUI
+
+struct MadisonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MadisonView()
+}
---
+++ Greetings/Views/MeiView.swift
@@ -0,0 +1,18 @@
+//
+//  MeiView.swift
+//  Greetings
+//
+//  Created by Rob Ranf on 2/3/25.
+//
+
+import SwiftUI
+
+struct MeiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeiView()
+}
